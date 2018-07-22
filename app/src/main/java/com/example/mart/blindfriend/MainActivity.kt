8536f1f55cceb175ded0be967f9f2e1a3548ddf0
Thanaphoom Babparn <mart-tp@hotmail.com>
2018-07-22 07:15:41 +0700

    [Commit 2] : Update CameraView

--- app/src/main/java/com/example/mart/blindfriend/MainActivity.kt
+++ app/src/main/java/com/example/mart/blindfriend/MainActivity.kt
@@ -1,36 +1,313 @@
 package com.example.mart.blindfriend
 
-import android.graphics.Camera
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CameraMetadata
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.TotalCaptureResult
+import android.hardware.camera2.params.StreamConfigurationMap
+import android.media.Image
+import android.media.ImageReader
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import android.util.Size
+import android.util.SparseIntArray
+import android.view.Surface
+import android.view.TextureView
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.nio.ByteBuffer
+import java.util.ArrayList
+import java.util.Arrays
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
-    var currentPath: String? = null
-    val CAMERA_REQUEST_CODE = 0
-    val GALLAERY_REQUEST_CODE = 1
 
+    private var cameraId: String? = null
+    private var cameraDevice: CameraDevice? = null
+    private var cameraCaptureSessions: CameraCaptureSession? = null
+    private var captureRequestBuilder: CaptureRequest.Builder? = null
+    private var imageDimension: Size? = null
+//    private val imageReader: ImageReader? = null
 
-    var options = FirebaseVisionCloudDetectorOptions.Builder()
-            .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
-            .setMaxResults(15)
-            .build()
+    //Save to FILE
+    private var file: File? = null
+//    private val mFlashSupported: Boolean = false
+    private var mBackgroundHandler: Handler? = null
+    private var mBackgroundThread: HandlerThread? = null
 
+    private var stateCallback: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            createCameraPreview()
+        }
 
-    lateinit var image: FirebaseVisionImage
+        override fun onDisconnected(cameraDevice: CameraDevice) {
+            cameraDevice.close()
+        }
+
+        override fun onError(cameraDevice: CameraDevice, i: Int) {
+            val cameraDevice = cameraDevice
+            cameraDevice.close()
+//            cameraDevice = null
+        }
+    }
+
+    private var textureListener: TextureView.SurfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surfaceTexture: SurfaceTexture, i: Int, i1: Int) {
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(surfaceTexture: SurfaceTexture, i: Int, i1: Int) {
+
+        }
+
+        override fun onSurfaceTextureDestroyed(surfaceTexture: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surfaceTexture: SurfaceTexture) {
+
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //From Java 1.4 , you can use keyword 'assert' to check expression true or false
+        assert(textureView != null)
+        textureView!!.surfaceTextureListener = textureListener
+        btnCapture!!.setOnClickListener { takePicture() }
+    }
 
-        buttonCapture.setOnClickListener {
+    private fun takePicture() {
+        if (cameraDevice == null)
+            return
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            val characteristics = manager.getCameraCharacteristics(cameraDevice!!.id)
+            var jpegSizes: Array<Size>? = null
+            if (characteristics != null)
+                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+                        .getOutputSizes(ImageFormat.JPEG)
 
+            //Capture image with custom size
+            var width = 640
+            var height = 480
+            if (jpegSizes != null && jpegSizes.isNotEmpty()) {
+                width = jpegSizes[0].width
+                height = jpegSizes[0].height
+            }
+            val reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1)
+            val outputSurface = ArrayList<Surface>(2)
+            outputSurface.add(reader.surface)
+            outputSurface.add(Surface(textureView!!.surfaceTexture))
+
+            val captureBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+            captureBuilder.addTarget(reader.surface)
+            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+
+            //Check orientation base on device
+            val rotation = windowManager.defaultDisplay.rotation
+            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation))
+
+            file = File(Environment.getExternalStorageDirectory().toString() + "/" + UUID.randomUUID().toString() + ".jpg")
+            val readerListener = object : ImageReader.OnImageAvailableListener {
+                override fun onImageAvailable(imageReader: ImageReader) {
+                    var image: Image? = null
+                    try {
+                        image = reader.acquireLatestImage()
+                        val buffer = image!!.planes[0].buffer
+                        val bytes = ByteArray(buffer.capacity())
+                        buffer.get(bytes)
+                        save(bytes)
+
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        run { image?.close() }
+                    }
+                }
+
+                @Throws(IOException::class)
+                private fun save(bytes: ByteArray) {
+                    var outputStream: OutputStream? = null
+                    try {
+                        outputStream = FileOutputStream(file!!)
+                        outputStream.write(bytes)
+                    } finally {
+                        outputStream?.close()
+                    }
+                }
+            }
+
+            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler)
+            val captureListener = object : CameraCaptureSession.CaptureCallback() {
+                override fun onCaptureCompleted(session: CameraCaptureSession, request: CaptureRequest, result: TotalCaptureResult) {
+                    super.onCaptureCompleted(session, request, result)
+                    Toast.makeText(this@MainActivity, "Saved " + file!!, Toast.LENGTH_SHORT).show()
+                    createCameraPreview()
+                }
+            }
+
+            cameraDevice!!.createCaptureSession(outputSurface, object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                    try {
+                        cameraCaptureSession.capture(captureBuilder.build(), captureListener, mBackgroundHandler)
+                    } catch (e: CameraAccessException) {
+                        e.printStackTrace()
+                    }
+
+                }
+
+                override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+
+                }
+            }, mBackgroundHandler)
+
+
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
         }
 
+    }
+
+    private fun createCameraPreview() {
+        try {
+            val texture = textureView!!.surfaceTexture!!
+            texture.setDefaultBufferSize(imageDimension!!.width, imageDimension!!.height)
+            val surface = Surface(texture)
+            captureRequestBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder!!.addTarget(surface)
+            cameraDevice!!.createCaptureSession(Arrays.asList(surface), object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                    if (cameraDevice == null)
+                        return
+                    cameraCaptureSessions = cameraCaptureSession
+                    updatePreview()
+                }
+
+                override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+                    Toast.makeText(this@MainActivity, "Changed", Toast.LENGTH_SHORT).show()
+                }
+            }, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun updatePreview() {
+        if (cameraDevice == null)
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        captureRequestBuilder!!.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO)
+        try {
+            cameraCaptureSessions!!.setRepeatingRequest(captureRequestBuilder!!.build(), null, mBackgroundHandler)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+
+    }
+
+
+    private fun openCamera() {
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            cameraId = manager.cameraIdList[0]
+            val characteristics = manager.getCameraCharacteristics(cameraId!!)
+            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            imageDimension = map.getOutputSizes(SurfaceTexture::class.java)[0]
+            //Check realtime permission if run higher API 23
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_CAMERA_PERMISSION)
+                return
+            }
+            manager.openCamera(cameraId!!, stateCallback, null)
+
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "You can't use camera without permission", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startBackgroundThread()
+        if (textureView!!.isAvailable)
+            openCamera()
+        else
+            textureView!!.surfaceTextureListener = textureListener
+    }
+
+    override fun onPause() {
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+    private fun stopBackgroundThread() {
+        mBackgroundThread!!.quitSafely()
+        try {
+            mBackgroundThread!!.join()
+            mBackgroundThread = null
+            mBackgroundHandler = null
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun startBackgroundThread() {
+        mBackgroundThread = HandlerThread("Camera Background")
+        mBackgroundThread!!.start()
+        mBackgroundHandler = Handler(mBackgroundThread!!.looper)
+    }
+
+    companion object {
+
+        //Check state orientation of output image
+        private val ORIENTATIONS = SparseIntArray()
+
+        init {
+            ORIENTATIONS.append(Surface.ROTATION_0, 90)
+            ORIENTATIONS.append(Surface.ROTATION_90, 0)
+            ORIENTATIONS.append(Surface.ROTATION_180, 270)
+            ORIENTATIONS.append(Surface.ROTATION_270, 180)
+        }
+
+        private val REQUEST_CAMERA_PERMISSION = 200
     }
 }
---
+++ app/src/main/java/com/example/mart/blindfriend/lexitronAccess.java
@@ -0,0 +1,70 @@
+package com.example.mart.blindfriend;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lexitronAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static lexitronAccess instance;
+
+    /**
+     * Private constructor to aboid object creation from outside classes.
+     *
+     * @param context
+     */
+    private lexitronAccess(Context context) {
+        this.openHelper = new lexitronDB(context);
+    }
+
+    /**
+     * Return a singleton instance of DatabaseAccess.
+     *
+     * @param context the Context
+     * @return the instance of DabaseAccess
+     */
+    public static lexitronAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new lexitronAccess(context);
+        }
+        return instance;
+    }
+
+    /**
+     * Open the database connection.
+     */
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    /**
+     * Close the database connection.
+     */
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    /**
+     * Read all quotes from the database.
+     *
+     * @return a List of quotes
+     */
+    public List<String> getQuotes() {
+        List<String> list = new ArrayList<>();
+        Cursor cursor = database.rawQuery("SELECT * FROM quotes", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+}
